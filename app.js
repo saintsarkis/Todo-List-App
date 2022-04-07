12c94c178b902c4ff11037bfd7dfb437905e3712
John Bryan <johnmorganbryan@gmail.com>
2022-04-06 23:39:48 -0400

    added some basic header, content, and footer styles, as well as a text box with a button

---
+++ app.js
@@ -0,0 +1,7 @@
+// now we have access to the header in JavaScript... we probably have no need for this
+const header = document.querySelector('header');
+// now we have access to the main section in JavaScript
+const body = document.querySelector('section');
+// now we have access to the footer in JavaScript... we'll most likely delete this later
+const footer = document.querySelector('footer');
+
