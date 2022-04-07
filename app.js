07e929f6076ac013e06a408ccbf90a6b60537d6f
John Bryan <johnmorganbryan@gmail.com>
2022-04-06 23:48:39 -0400

    added header, content, footer, and textbox/button thingy...

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-// now we have access to the header in JavaScript... we probably have no need for this
-const header = document.querySelector('header');
-// now we have access to the main section in JavaScript
-const body = document.querySelector('section');
-// now we have access to the footer in JavaScript... we'll most likely delete this later
-const footer = document.querySelector('footer');
-
+/**
+ * Let's get a reference to the button and input... maybe we can use
+ * these references to listen for clicks on the button and update our list
+ */
+const button = document.querySelector('#todo-form button');
+const input = document.querySelector('#todo-form input');
