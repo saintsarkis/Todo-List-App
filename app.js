802d78f5aa1a4259696d3e0d8ee778779eba96c9
Benjamin J Braden <benbraden@Benjamins-MacBook-Pro-2.local>
2022-04-12 14:38:41 -0400

    added click and keypress implememnted to add items to list

--- app.js
+++ app.js
@@ -4,3 +4,26 @@
  */
 const button = document.querySelector('#todo-form button');
 const input = document.querySelector('#todo-form input');
+const ul = document.querySelector('ul');
+
+button.addEventListener('click', () => {
+    if (input.value.length > 0) {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        console.log(input.value);
+        input.value = "";
+    }
+});
+
+input.addEventListener('keypress', (event) => {
+    if (input.value.length > 0 && event.which === 13) {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        console.log(input.value);
+        input.value = "";
+    }
+});
+
+
